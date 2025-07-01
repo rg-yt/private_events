7fbd310f9221176ac305ad15149b0f189de1e7d8
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-07-01 12:51:05 -0400

    Add path to event index and basic data
    Added root route to point to events index page and
    added a basic list to show all event titles

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_01_163519) do
+  create_table "events", force: :cascade do |t|
+    t.date "date"
+    t.time "time"
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
