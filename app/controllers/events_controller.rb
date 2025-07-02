c332e6338087a47f6602a3383bc32a9a965b593b
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-07-01 22:18:36 -0400

    Update controller with new, create, and show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,29 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [ :new, :create ]
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.expect(event: [ :title, :description, :time, :date ])
   end
 end
